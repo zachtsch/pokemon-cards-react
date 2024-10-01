700be0ca50bd2736d65b056362d707d0539f9989
zachtschetter <zachary.tschetter@usd.edu>
2024-10-01 15:07:18 -0500

    background colors

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -1,5 +1,27 @@
 import React, { useEffect, useState } from "react";
 
+// Color mapping for Pokémon types
+const typeColors = {
+  grass: "#78C850",
+  fire: "#F08030",
+  water: "#6890F0",
+  bug: "#A8B820",
+  normal: "#A8A878",
+  poison: "#A040A0",
+  electric: "#F8D030",
+  ground: "#E0C068",
+  fairy: "#EE99AC",
+  fighting: "#C03028",
+  psychic: "#F85888",
+  rock: "#B8A038",
+  ghost: "#705898",
+  ice: "#98D8D8",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  flying: "#A890F0",
+};
+
 const PokemonCard = ({ pokemonId }) => {
   const [pokemonData, setPokemonData] = useState(null);
 
@@ -21,8 +43,14 @@ const PokemonCard = ({ pokemonId }) => {
     return <div>Loading...</div>;
   }
 
+  // Get the primary type of the Pokémon
+  const primaryType = pokemonData.types[0].type.name;
+
+  // Set the background color based on the primary type
+  const backgroundColor = typeColors[primaryType] || "#f8f8f8"; // default color if type is not in typeColors
+
   return (
-    <div style={styles.card}>
+    <div style={{ ...styles.card, backgroundColor }}>
       <img src={pokemonData.sprites.front_default} alt={pokemonData.name} style={styles.image} />
       <h3>{pokemonData.name}</h3>
       <p>Type: {pokemonData.types.map((typeInfo) => typeInfo.type.name).join(", ")}</p>
@@ -40,7 +68,6 @@ const styles = {
     textAlign: "center",
     width: "150px",
     margin: "10px",
-    backgroundColor: "#f8f8f8",
   },
   image: {
     width: "100px",
